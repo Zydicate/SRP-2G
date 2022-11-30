201cf6f2631d57ecea94a4dc628af9943bcc746f
Zydicate <111048662+Zydicate@users.noreply.github.com>
2022-11-30 21:30:28 +0100

    rework eftersom koden var dårligt organiseret og ikke ibrug af physics engine til at lave collisions

--- SRC-JUMP KING.py
+++ SRC-JUMP KING.py
@@ -1,95 +1,108 @@
 import arcade
-import math
 
-BREDDE = 800
-HOEJDE = 600
-TITEL = "Jump King 2.0"
+SCREEN_BREDDE = 800
+SCREEN_HOEJDE = 600
+SCREEN_TITLE = "Platformer"
 
-#Skaler karakter billederne til spillet
+# Constants used to scale our sprites from their original size
 KARAKTER_SKALERING = 1
-JORD_SKALERING = 0.2
-#Karakter hastighed
-PLAYER_MOVEMENT_SPEED = 5
+TILE_SCALING = 0.5
+
+# Movement speed of player, in pixels per frame
+KARAKTER_HASTIGHED = 5
+
+TYNGDEKRAFT = 1
+
+KARAKTER_HOPPEHASTIGHED = 20
 
 
-#3. Platformen
-class Platform:
-    def __init__(self, center_x, center_y, width, height, farve):
-        self.center_x = center_x
-        self.center_y = center_y
-        self.width = width
-        self.height = height
-        self.farve = farve
 
-    #
-    def tegn(self):
-        arcade.draw_rectangle_filled(BREDDE/2, HOEJDE/10-50,BREDDE,100, arcade.csscolor.DIM_GREY)
+class JumpKing(arcade.Window):
 
-#2. Starter vores vindue
-class Vindue(arcade.Window):
-    def __init__(self, width, height, title):
-        #Returner et objekt som symbolisere / repræsentere forældreren
-        super().__init__(BREDDE,HOEJDE,TITEL)
+    def __init__(self):
+        super().__init__(SCREEN_BREDDE, SCREEN_HOEJDE, SCREEN_TITLE)
+        #Vi nød til at sætte en scene i gang
+        self.scene = None
+        self.player_sprite = None
+        #vores sprite / karakter
+        #physics engine som skal bruges til colission og tyngdekraft
+        self.physics_engine = None
+        arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
 
-
-        self.wall_list = None
-        self.player_list = None
-
-        #Vi brug
-        self.physics_engine = arcade.PhysicsEngineSimple(
-            self.player_sprite, self.scene.get_sprite_list("Walls")
-        )
-
-        arcade.set_background_color(arcade.csscolor.BLACK)
-
-        #Loader baggrunden
         self.background = arcade.load_texture("BACKGROUND STAR.jpg")
 
-
-
-    #brugt når jeg skal tegne mit vindue
-    def on_draw(self):
-        #begynd at tegne
-        arcade.start_render()
-        arcade.draw_texture_rectangle(BREDDE / 2, HOEJDE / 2, BREDDE, HOEJDE, self.background)
-        self.platform.tegn()
-        #laver textur til platform. JPG bliver distorted hvis bredden er for stor så vi duplikere den 6 gange.
-
-        #
-
-        self.wall_list.draw()
-        self.player_list.draw()
-
-        #
-
     def setup(self):
-        self.platform = Platform(BREDDE, HOEJDE/2,50,100, arcade.csscolor.BROWN)
-        #Lave en liste med sprites
-        self.player_list = arcade.SpriteList()
-        self.wall_list = arcade.SpriteList(use_spatial_hash=True)
-
-        #Sætter en spiller ind. Billedet er taget fra https://api.arcade.academy/en/latest/resources.html
+        # vi starter en scene
+        self.scene = arcade.Scene()
         image_source = ":resources:images/animated_characters/zombie/zombie_idle.png"
+        #karaktere som er oploaded fra https://api.arcade.academy/en/latest/resources.html
         self.player_sprite = arcade.Sprite(image_source, KARAKTER_SKALERING)
         self.player_sprite.center_x = 64
-        self.player_sprite.center_y = 124
-        self.player_list.append(self.player_sprite)
-
-        #Indsætte jorden i en range
-        for x in range(0, BREDDE+200, 150):
-            wall = arcade.Sprite("BLOCK BILLED MC.png", JORD_SKALERING)
+        self.player_sprite.center_y = 128
+        self.scene.add_sprite("Player", self.player_sprite)
+        #vi laver vores karakter og indsætter
+        #Vi laver jorden. Med en x værdi fra 0-1250 og intervallet
+        for x in range(0, 1250, 64):
+            wall = arcade.Sprite("BLOCK BILLED MC.png", 0.1)
             wall.center_x = x
-            wall.center_y = -12
-            self.wall_list.append(wall)
+            wall.center_y = 32
+            self.scene.add_sprite("Walls", wall)
+
+        #Så vil jeg lave nogle platforme
+        coordinate_list = \
+            [[512, 220],
+             [256, 220]]
+
+        for coordinate in coordinate_list:
+            # Laver platformene
+            wall = arcade.Sprite(
+                "STEN PLATFORM.png", 0.1
+            )
+            wall.position = coordinate
+            self.scene.add_sprite("Walls", wall)
 
 
+        # Create the 'physics engine'
+
+        self.physics_engine = arcade.PhysicsEnginePlatformer(
+
+            self.player_sprite, gravity_constant=TYNGDEKRAFT, walls=self.scene["Walls"]
+
+        )
+    #karakter bevægelse
+    def on_key_press(self, key, modifiers):
+        if key == arcade.key.UP or key == arcade.key.W:
+            if self.physics_engine.can_jump():
+                self.player_sprite.change_y = KARAKTER_HOPPEHASTIGHED
+        elif key == arcade.key.A:
+            self.player_sprite.change_x = -KARAKTER_HASTIGHED
+        elif key == arcade.key.D:
+            self.player_sprite.change_x = KARAKTER_HASTIGHED
+        # så karakteren ikke bliver ved med at bevæge sig i den retning
+
+    def on_key_release(self, key, modifiers):
+        if key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = 0
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = 0
+    def on_draw(self):
+        #tegner skærmen og frigøre den
+        self.clear()
+        arcade.draw_texture_rectangle(SCREEN_BREDDE / 2, SCREEN_HOEJDE / 2, SCREEN_BREDDE, SCREEN_HOEJDE,
+                                      self.background)
+        self.scene.draw()
 
 
-#1.Først sætter jeg et main op som jeg kan bruge til at køre hele mit program. Dette starter selvfølgelig med at sætte et arcade window op
+    def on_update(self, delta_time):
+        self.physics_engine.update()
+        #opdatere den
+
+#main funktion hvor vi køre det hele
 def main():
-    vindue = Vindue(BREDDE, HOEJDE, TITEL)
-    vindue.setup()
-
-
+    window = JumpKing()
+    window.setup()
     arcade.run()
-main()
+
+
+if __name__ == "__main__":
+    main()
